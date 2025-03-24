75864a81f2fcf305486957b4e7e4f908452dd034
mishal muskan <163289413+meshalmuskan@users.noreply.github.com>
2025-03-24 22:53:24 +0500

    fetch and display trending and top-rated movies

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,20 @@
-import Image from "next/image";
+import Results from '../components/Results';
+const API_KEY= process.env.API_KEY;
+import React from 'react'
 
-export default function Home() {
+export default async function Home() {
+  const res = await fetch(`https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US&page=1`);
+  const data= await res.json();
+  if(!res.ok){
+    throw new Error('Failed to fetch data');
+
+  }
+  const result= data.results;
   return (
-    <div className="text-red-700">home</div>
-  );
+    <div>
+      <Results results={result}/>
+      
+    </div>
+  )
 }
+
